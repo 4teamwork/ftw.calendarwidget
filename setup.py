6b4e463ba3d7fe29926d668def0e56bda8706566
Jonas Baumann <jone@jone.ch>
2011-11-30 18:02:40 +0100

    Add description.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ maintainer = 'Philippe Gross'
 
 setup(name='ftw.calendarwidget',
       version=version,
-      description="",
+      description="A plone widget for archetypes using the jQuery UI calendar widget",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
